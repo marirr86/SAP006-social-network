e0f4e7171d82a81be6831d0d33a2f801d6eb9a41
marirr86 <rr.mari@hotmail.com>
2021-09-01 18:49:06 -0300

    conf

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,8 +1,17 @@
 export const feed = () => {
   const div = document.createElement('div');
-  const content = `
-        <h1> Feed </h1>
-        
+  const content = ` 
+    <div>container-feed
+      <div class="head-feed">
+        <span>casinha</span>
+        <h1 class="title-feed">Homescreen</h1>
+      </div>
+        <div class="post-area">
+          <div class="post-area-in"> 
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+          </div>
+      </div>
+    </div>   
       `;
 
   div.innerHTML = content;
