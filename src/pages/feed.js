2c7a699fc022d5094a09835d4583c3d85c5d47bd
marirr86 <rr.mari@hotmail.com>
2021-09-01 22:27:59 -0300

    feed: html/css

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,17 +1,23 @@
 export const feed = () => {
   const div = document.createElement('div');
   const content = ` 
-    <div>container-feed
-      <div class="head-feed">
+    <div class="container-feed">
+
+      <header class="head-feed">
         <span>casinha</span>
         <h1 class="title-feed">Homescreen</h1>
+      </header>
+
+      <div class="post-area">
+        <form action="" class="form-post">
+          <label class="review-label" for="book-name">Add a new review</label>
+          <input class="post-place" type="text" placeholder="Visited place">
+          <textarea class="post-review" id="" cols="50" rows="10" placeholder="Describe your experiences"></textarea>
+          <button type='submit' class='submit' id='submit-post'>Submit</button>
+        </form>
       </div>
-        <div class="post-area">
-          <div class="post-area-in"> 
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-          </div>
-      </div>
-    </div>   
+    
+    </div>
       `;
 
   div.innerHTML = content;
