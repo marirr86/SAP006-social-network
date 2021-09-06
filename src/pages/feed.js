fdd75eb508a7c6d3fdd1f95ac1b962edf2d0f627
RAFAELA <72530170+RafaelaCugini@users.noreply.github.com>
2021-09-06 15:57:45 -0300

    creating post

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,3 +1,5 @@
+import { postarMensagem } from '../services/index.js';
+
 export const feed = () => {
   const div = document.createElement('div');
   const content = ` 
@@ -11,17 +13,39 @@ export const feed = () => {
         <form action="" class="form-post">
           <label class="review-label" for="book-name">Add a new review</label>
           <input class="post-place" type="text" placeholder="Visited place">
-          <textarea class="post-review" id="" cols="50" rows="10" placeholder="Describe your experiences"></textarea>
-          <button type='submit' class='submit' id='submit-post'>Submit</button>
+          <textarea class="post-review" id="mensagem" cols="50" rows="10" placeholder="Describe your experiences"></textarea>
+          <button type='button' class='submit' id='submit-post'>Submit</button>
         </form>
       </div>
     
     </div>
       `;
 
-  div.innerHTML = content;
+    div.innerHTML = content;
+
+    const btn = div.querySelector('#submit-post');
+    btn.addEventListener('click' , () => {
+      const msg = div.querySelector('#mensagem').value;
+      
+      const postagem = {
+        mensagem: msg,
+        autora: 'Rafaela',
+      };
+
+      postarMensagem(postagem)
+        .then((docRef) => {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch((error) => {
+        console.error("Error adding document: ", error);
+      });
+    });
+
+  
   return div;
 };
+
+
 
 /* firebase.auth().signOut().then(() => {
   // Sign-out successful.
