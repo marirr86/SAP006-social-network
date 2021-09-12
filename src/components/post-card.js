4b5d52fdce0429cf18a7834c7e9b7fd6842fa0de
marirr86 <rr.mari@hotmail.com>
2021-09-12 19:29:15 -0300

    edit, del

--- src/components/post-card.js
+++ src/components/post-card.js
@@ -1,4 +1,6 @@
-import { likePost, deslikePost } from '../services/index.js';
+import {
+  likePost, deslikePost, delPost, editPost,
+} from '../services/index.js';
 
 export const card = (objPost) => {
   const idUser = firebase.auth().currentUser.uid;
@@ -12,14 +14,18 @@ export const card = (objPost) => {
   const contentCard = ` 
       <h2>${objPost.autora}</h2>
       <p>Visited Place: ${objPost.local}</p>
-      <p>${objPost.mensagem}</p>
+      <div data-text="${objPost.id}">${objPost.mensagem}
+      </div>
       <button class="like-button ${classLike}" data-like="${objPost.id}">like</button>
+      <button class="del-button" data-del="${objPost.id}">delete</button>
+      <button class="edit-button" data-edit="${objPost.id}">Edit</button>
+      <button class="save-button" data-save="${objPost.id}">Save</button>
         `;
 
   infoDiv.innerHTML = contentCard;
-  const likeBtn = infoDiv.querySelector(`[data-like="${objPost.id}"]`);
-  // console.log(likeBtn);
 
+  // LIKE / DESLIKE
+  const likeBtn = infoDiv.querySelector(`[data-like="${objPost.id}"]`);
   likeBtn.addEventListener('click', () => {
     if (objPost.like.includes(idUser)) {
       deslikePost(idUser, objPost.id).then(() => {
@@ -30,6 +36,30 @@ export const card = (objPost) => {
         window.location.reload();
       });
     }
+  });
+
+  // DELETE
+  const delBtn = infoDiv.querySelector(`[data-del="${objPost.id}"]`);
+  delBtn.addEventListener('click', () => {
+    delPost(objPost.id).then(() => {
+      window.location.reload();
+    });
+  });
+
+  // UPDATE
+  const editMsg = infoDiv.querySelector(`[data-text="${objPost.id}"]`);
+  const editBtn = infoDiv.querySelector(`[data-edit="${objPost.id}"]`);
+  editBtn.addEventListener('click', () => {
+    console.log(editMsg);
+    editMsg.setAttribute('contentEditable', '');
+    editMsg.focus();
+  });
+
+  const saveBtn = infoDiv.querySelector(`[data-save="${objPost.id}"]`);
+  saveBtn.addEventListener('click', () => {
+    editPost(objPost.id, editMsg.innerText).then(() => {
+      editMsg.removeAttribute('contentEditable', '');
+    });
   });
 
   return infoDiv;
